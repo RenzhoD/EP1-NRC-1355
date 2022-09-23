c5ba6fb80fd9fb64946b625ece87f5cdf5c0a0bd
RenzhoD <renzhohs@gmail.com>
2022-09-23 17:34:44 -0500

    Proyecto terminado

--- src/main/java/pe/isil/EP1/Application.java
+++ src/main/java/pe/isil/EP1/Application.java
@@ -3,11 +3,37 @@ package pe.isil.EP1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         SpringApplication.run(Application.class, args);
-    }
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/EP1",
+                "root","");
+
+        CallableStatement callableStatement = conexion.prepareCall("{call listarRegistros()}");
+        ResultSet resultSet = callableStatement.executeQuery();
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("id"));
+            System.out.println(resultSet.getString("name"));
+            System.out.println(resultSet.getString("username"));
+            System.out.println(resultSet.getString("tipDoc"));
+            System.out.println(resultSet.getString("nroDoc") + "\n");
+        }
+
+        PreparedStatement preparedStatement = conexion.prepareStatement("update users set name= ? where name = ?");
+        preparedStatement.setString(1,"Juan");
+        preparedStatement.setString(2,"Bruno");
+        int resultSet1 = preparedStatement.executeUpdate();
+        System.out.println("Filas afectadas: " + resultSet1 + "\n");
+
+        Statement statement = conexion.createStatement();
+        int filasAfectadas = statement.executeUpdate("delete from users where id= 2");
+        System.out.println("Filas afectadas: " + filasAfectadas);
+    }
 }
--- src/main/resources/scripts/base_script.sql
+++ src/main/resources/scripts/base_script.sql
@@ -1,54 +1,27 @@
-show tables;
-
-
-/*Crear tabla*/
-CREATE TABLE USERS (
-
+create table users (
 	id int,
 	name varchar(255),
-	lastname varchar(255),
 	username varchar(20),
-	pass    varchar(100),
 	tipDoc  char(5),
-	nroDoc  varchar(20),
-	enable  int
+	nroDoc  varchar(20)
 )
 
-/* Añadimos id como primary Key*/
-ALTER TABLE USERS ADD PRIMARY KEY (id) ;
+alter table users add primary key (id);
 
-/* Hacemos el campo id autoincremental y que no permita registros null*/
-ALTER TABLE USERS MODIFY COLUMN id int auto_increment NOT NULL;
+alter table users MODIFY column id int auto_increment not null;
 
+INSERT INTO USERS (name,username,tipDoc,nroDoc)
+VALUES ('Jorge','DNI73267572','DNI','73267572');
 
+INSERT INTO USERS (name,username,tipDoc,nroDoc)
+VALUES ('Maria','DNI46299021','DNI','46299021');
 
+INSERT INTO USERS (name,username,tipDoc,nroDoc)
+VALUES ('Oliver','DNI10054725','DNI','10054725');
 
+INSERT INTO USERS (name,username,tipDoc,nroDoc)
+VALUES ('Bruno','DNI72584671','DNI','72584671');
 
-/*Insertar*/
-INSERT INTO USERS (name,lastname,username,pass,tipDoc,nroDoc,enable)
-VALUES ('Jose','Zuniga','DNI73267572','123456','DNI','73267572',1);
+update users set name='Matt' where username ='DNI72584671'
 
-
-INSERT INTO USERS (name,lastname,username,pass,tipDoc,nroDoc,enable)
-VALUES ('Maria','Santillan','DNI46299021','123456','DNI','46299021',1);
-
-
-
-/*Actualizar*/
-UPDATE USERS SET name='Mariana' WHERE username ='DNI46299021';
-
-
-/*Eliminar*/
-DELETE FROM USERS WHERE username='DNI73267572';
-
-
-/*Consultar*/
-SELECT * FROM USERS
-
-
-
-
-
-
-
-
+select * from users
